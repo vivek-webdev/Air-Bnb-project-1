b098b8087308c04dc01e596ac32ab0ec6cf2443f
Ashish <vsprajapati2810@gmail.com>
2025-09-06 14:51:01 +0530

    Added data and index.js and some ejs

--- app.js
+++ app.js
@@ -3,6 +3,8 @@ const app = express();
 const ejs = require("ejs");
 const mongoose = require('mongoose');
 const Lisiting = require("./models/listing.js")
+const path = require("path");
+const Listing = require("./models/listing.js");
 
 main().catch(err => console.log(err));
 
@@ -12,26 +14,40 @@ async function main() {
   // use `await mongoose.connect('mongodb://user:password@127.0.0.1:27017/test');` if your database has auth enabled
 }
 
-
+app.set("view engine","ejs")
+app.set("views",path.join(__dirname,"views"))
+app.use(express.urlencoded({extended:true}))
 
 app.get("/",(req,res) => {
     res.send("working")
 })
 
-app.get("/testListening",async (req,res) => {
-    sampleListing = new Lisiting({
-    title : "andabam nicobar",
-    description : "nice island",
-    price : 10009,
-    location : "somewhere in the ocean",
-    country : "India"
-  })
 
-  await sampleListing.save();
-  console.log("data is saved")
-  res.send("db is wokring")
-
+//Index route
+app.get("/listing", async (req,res) => {
+  const allListings = await Lisiting.find({})
+  res.render("./listings/index.ejs",  { allListings })
 })
+
+//Show Route
+app.get("/listings/:id", async (req, res) => {
+  let { id } = req.params;
+  const listing = await Listing.findById(id);
+  res.render("listings/show.ejs", { listing });
+});
+
+// app.get("/testListening",async (req,res) => {
+//     sampleListing = new Lisiting({
+//     title : "andabam nicobar",
+//     description : "nice island",
+//     price : 10009,
+//     location : "somewhere in the ocean",
+//     country : "India"
+//   })
+//   await sampleListing.save();
+//   console.log("data is saved")
+//   res.send("db is wokring")
+// })
 
 app.listen(8080 , ()=> {
     console.log("wrver is live")
--- models/listing.js
+++ models/listing.js
@@ -1,20 +1,26 @@
 const mongoose = require("mongoose");
 const Schema = mongoose.Schema;
 
-const listingSchema = new Schema ({
-    title :{ 
-        type :String,
-        required : true
+const listingSchema = new Schema({
+  title: { 
+    type: String,
+    required: true
+  },
+  description: String,
+  image: {
+    filename: {
+      type: String,
+      default: "listingimage"
     },
-    description : String,
-    image :{
-        type : String,
-        default : "https://images.unsplash.com/photo-1755371034010-51c25321312d?q=80&w=2070&auto=format&fit=crop&ixlib=rb-4.1.0&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D",
-    },
-    price : Number,
-    location : String,
-    country : String
-})
+    url: {
+      type: String,
+      default: "https://images.unsplash.com/photo-1755371034010-51c25321312d?q=80&w=2070&auto=format&fit=crop&ixlib=rb-4.1.0&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D"
+    }
+  },
+  price: Number,
+  location: String,
+  country: String
+});
 
-const Lisiting = mongoose.model("Listing",listingSchema);
-module.exports = Lisiting;
+const Listing = mongoose.model("Listing", listingSchema);
+module.exports = Listing;
